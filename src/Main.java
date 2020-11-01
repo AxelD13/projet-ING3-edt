c79d516c3584e33d053299b215f1ff4c3ba5c4fb
Axel DERDOUR <axel.derdour@edu.ece.fr>
2020-11-01 16:58:08 +0100

    2e commit (MAJ du main pour Windows et Mac)

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,18 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver ok.");
 
+            //Pour Windows :
             String url = "jdbc:mysql://localhost:3306/projet_edt";
             String user = "root";
             String password = "";
+
+            //Pour Mac :
+            /*
+            String url = "jdbc:mysql://localhost:8889/projet_edt";
+            String user = "root";
+            String password = "root";
+            */
+
             Connection cnx = DriverManager.getConnection(url, user, password);
 
             System.out.println("Connexion établie.");
